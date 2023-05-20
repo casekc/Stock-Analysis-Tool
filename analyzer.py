83588b43d777a8f3e8aef2fdf3b6db87af858d7e
PoliticsOnly <68171370+PoliticsOnly@users.noreply.github.com>
2023-05-19 20:56:57 -0500

    In formatting.py module, added convertCSV() method to convert historical price .csv files to .xlsx.

--- analyzer.py
+++ analyzer.py
@@ -2,9 +2,10 @@ from openpyxl import Workbook
 from openpyxl import load_workbook
 from extractor import stockInput
 
+
 def analyzeNetIncome(ticker_list):
     # Loads the net income workbook
-    wb = load_workbook(filename = r'C:\Users\cummi\Desktop\webscrap\bin\net_income.xlsx')
+    wb = load_workbook(filename=r'C:\Users\cummi\Desktop\webscrap\bin\net_income.xlsx')
 
     # Loads the first sheet of the net income workbook
     ws = wb.active
@@ -17,7 +18,7 @@ def analyzeNetIncome(ticker_list):
     ws['I1'] = 'AAGR'
 
     # Iterates over each row and writes the growth rate formulas
-    for i in range(2, len(ticker_list)+2):
+    for i in range(2, len(ticker_list) + 2):
         i = str(i)
         ws['F' + i] = '=((D' + i + '- E' + i + ')/E' + i + ')*100'
         ws['G' + i] = '=((C' + i + '- D' + i + ')/D' + i + ')*100'
@@ -28,19 +29,33 @@ def analyzeNetIncome(ticker_list):
     wb.save(r'C:\Users\cummi\Desktop\webscrap\bin\net_income_analyses\net_income_analysis.xlsx')
 
 
+
+ticker_list = stockInput()
+
 def analyzeHistoricalPrices(ticker_list):
-
+    xlsx_path = 'C:\\Users\\cummi\\Desktop\\webscrap\\bin\\xlsx\\'
+    historical_analysis_path = 'C:\\Users\\cummi\\Desktop\\webscrap\\bin\\historical_price_analyses\\'
     for ticker in ticker_list:
-        wb = load_workbook(r'C:\Users\cummi\Desktop\webscrap\bin\csv\' + ' + ticker + '.csv')
-
+        # Create a new workbook
+        wb = load_workbook(xlsx_path + ticker + '.xlsx')
         ws = wb.active
 
-        ws['F1'] = 'Pco'
-        ws['G1'] = 'Phl'
+        length = int(ws.max_row)
 
-        for i in range(2, len(ticker_list) + 2):
+        ws['G1'] = 'Pco'
+        ws['H1'] = 'Phl'
+        ws['I1'] = 'AvgPco'
+        ws['J1'] = 'AvgPhl'
+
+        for i in range(2, length):
             i = str(i)
-            ws['F' + i] = '=F'
-            ws['G' + i] = '=((C' + i + '- D' + i + ')/D' + i + ')*100'
-            ws['H' + i] = '=((B' + i + '- C' + i + ')/C' + i + ')*100'
-            ws['I' + i] = '=AVERAGE(F' + i + ':H' + i + ')'
+            ws['G' + i] = '=B' + i + '-D' + i  # B2-D2
+            ws['H' + i] = '=E' + i + '-F' + i  # E2-F2
+
+        ws['I2'] = '=AVERAGE(G2:G' + str(length)
+        ws['J2'] = '=AVERAGE(H2:H' + str(length)
+
+        wb.save(historical_analysis_path + ticker + '.xlsx')
+
+analyzeHistoricalPrices(ticker_list)
+
