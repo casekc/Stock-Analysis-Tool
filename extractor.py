5b6223de38de6157f92ba7f87888e03808519d1a
Case <110580012+casekc@users.noreply.github.com>
2023-08-31 23:04:42 -0500

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -1,4 +1,3 @@
-# imports
 import time
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -19,37 +18,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 
-
-
-# Setting Chrome driver options
-#options = webdriver.ChromeOptions()
-#prefs = {"download.default_directory": "C:\\Users\\cummi\\Desktop\\webscrap\\bin\\csv"}
-
-#options.add_experimental_option("prefs", prefs)
-
-def setup_chrome_driver():
-    # Specify the full path to chromedriver.exe
-    driver_path = r'C:\path\to\chromedriver.exe'  # Replace with the actual path to chromedriver.exe
-
-    # Set up the Chrome options
-    options = webdriver.ChromeOptions()
-
-    # Specify the default download directory
-    prefs = {"download.default_directory": "C:\\Users\\cummi\\Desktop\\webscrap\\bin\\csv"}
-    options.add_experimental_option("prefs", prefs)
-
-    # Set up the Service object with the executable path
-    service = Service(executable_path=driver_path)
-
-    # Set up the Chrome driver with the Service and options
-    driver = webdriver.Chrome(service=service, options=options)
-
-    # Return the driver object
-    return driver
-
-# Call the function to set up the Chrome driver
-
-
 def stockInput():
     true = True
 
@@ -67,30 +35,6 @@ def stockInput():
     return ticker_list
 
 
-def old_extractFinancials(ticker):
-    financials_url = 'https://www.nasdaq.com/market-activity/stocks/' + ticker + '/financials'
-    options = Options()
-
-    with webdriver.Chrome(options=options) as driver:
-        driver.get(financials_url)
-
-        while True:
-            try:
-                element = driver.find_element(By.XPATH, '//div[@class="financials__cell"]')
-                break
-            except NoSuchElementException:
-                pass
-
-            time.sleep(1)  # Wait for 1 second before checking again
-
-        resp = driver.page_source
-
-    soup = BeautifulSoup(resp, 'html.parser')
-    numbers_with_class = soup.find_all(class_='financials__cell')
-    numbers_list = [number.get_text(strip=True) for number in numbers_with_class]
-
-    return numbers_list
-
 def extractFinancials(ticker):
     financials_url = 'https://www.nasdaq.com/market-activity/stocks/' + ticker + '/financials'
     driver = webdriver.Chrome()
@@ -104,24 +48,6 @@ def extractFinancials(ticker):
 
     return numbers_list
 
-def old_extractHistorical(ticker):
-    historical_url = 'https://www.nasdaq.com/market-activity/stocks/' + ticker + '/historical'
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")  # Run Chrome in headless mode
-    service = Service('path_to_chromedriver')  # Replace with the actual path to chromedriver
-
-    with webdriver.Chrome(service=service, options=options) as driver:
-        driver.get(historical_url)
-        wait = WebDriverWait(driver, 10)
-
-        max_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@class="table-tabs__tab" and @data-value="y10"]')))
-        max_button.click()
-
-        action = ActionChains(driver)
-        download_button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'historical-data__controls-button--download')))
-        action.move_to_element(download_button).click().perform()
-
-        # Add additional waits or conditions as needed before closing the driver
 
 
 
